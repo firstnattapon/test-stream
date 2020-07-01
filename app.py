ea0f17e79f8b8ca66cb50ec9f1bae0736ced9507
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-02 04:08:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,11 +72,11 @@ class Run_model :
     
     @property 
     def Chart (self):
-        dataset = self.deep
+        dataset = self.deep ; close = self.dataset
         dataset['buy'] =  dataset.Predict.map(lambda x : np.where( x == True , x , None))
         dataset['sell'] = dataset.Predict.map(lambda x : np.where( x == False, x , None))
         plt.figure(figsize=(12,8))
-        plt.plot(dataset.close[-100:] , color='k' , alpha=0.20 )
+        plt.plot(close.close[-100:] , color='k' , alpha=0.20 )
         plt.plot(dataset.buy[-100:] , 'o',  color='g' , alpha=0.50 )
         plt.plot(dataset.sell[-100:] , 'o', color='r' , alpha=0.50)              
         st.pyplot()    
