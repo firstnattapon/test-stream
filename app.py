93738ec116c3740ce204941ff38acdc6b01b9fde
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-02 06:14:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,8 +77,8 @@ class Run_model :
         dataset['sell'] = dataset.apply(lambda x : np.where( x.Predict == False, x.close , None) , axis=1)
         plt.figure(figsize=(12,8))
         plt.plot(dataset.close[-100:] , color='k' , alpha=0.20 )
-        plt.plot(dataset.buy[-100:] , 'o',  color='g' , alpha=0.50 )
-        plt.plot(dataset.sell[-100:] , 'o', color='r' , alpha=0.50)              
+#         plt.plot(dataset.buy[-100:] , 'o',  color='g' , alpha=0.50 )
+#         plt.plot(dataset.sell[-100:] , 'o', color='r' , alpha=0.50)              
 #         plt.show()
      
     @property 
