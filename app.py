7ed6cbc02f973c2aeb1671146fc5ac12393b093f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-02 05:03:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,22 +75,22 @@ class Run_model :
         dataset = self.deep
         dataset['buy'] =  dataset.apply(lambda x : np.where( x.Predict == True , x.close , None) , axis=1)
         dataset['sell'] = dataset.apply(lambda x : np.where( x.Predict == False, x.close , None) , axis=1)
-        plt.figure(figsize=(12,8))
+#         plt.figure(figsize=(12,8))
         plt.plot(dataset.close[-100:] , color='k' , alpha=0.20 )
         plt.plot(dataset.buy[-100:] , 'o',  color='g' , alpha=0.50 )
         plt.plot(dataset.sell[-100:] , 'o', color='r' , alpha=0.50)              
-        st.pyplot()    
+        st.pyplot(figsize=(12,8))    
 
     @property
     def Chart2 (self):
         dataset = self.deep
         dataset['buy'] =  dataset.apply(lambda x : np.where( x.Predict == True , x.close , None) , axis=1)
         dataset['sell'] = dataset.apply(lambda x : np.where( x.Predict == False, x.close , None) , axis=1)
-        plt.figure(figsize=(12,8))
+#         plt.figure(figsize=(12,8))
         plt.plot(dataset.close[-100:] , color='k' , alpha=0.20 )
         plt.plot(dataset.buy[-100:] , 'o',  color='g' , alpha=0.50 )
         plt.plot(dataset.sell[-100:] , 'o', color='r' , alpha=0.50)              
-        st.pyplot()         
+        st.pyplot(figsize=(12,8))         
         
     @property 
     def  trade (self):
