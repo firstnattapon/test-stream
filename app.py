8985eca3c78a8a3007e5159fa26e62219672b8be
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-03 00:18:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -146,10 +146,18 @@ class Run_model :
 #         model =  Run_model()
 #         model.trade
 
+model =  Run_model()
+model.timeframe = st.sidebar.selectbox('timeframe',('15m' , '5m' ,  '1h', '4h' ,'1d' ))
+model.start_test =  np.datetime64(st.sidebar.date_input('start_test', value= dt.datetime(2020, 7, 2, 0, 0)))
+pyplot = model.chart
+st.sidebar.text('Predict: {}'.format(model.deep.Predict[-1]))
+st.sidebar.text('Nav: {}'.format(round(model.nav.Cumulative_Strategy_Returns[-2]) , 3 ))
+
+
 if st.sidebar.button('Refresh'):
     model =  Run_model()
     model.timeframe = st.sidebar.selectbox('timeframe',('15m' , '5m' ,  '1h', '4h' ,'1d' ))
     model.start_test =  np.datetime64(st.sidebar.date_input('start_test', value= dt.datetime(2020, 7, 2, 0, 0)))
     pyplot = model.chart
     st.sidebar.text('Predict: {}'.format(model.deep.Predict[-1]))
-    st.sidebar.text('Nav: {}'.format(round(model.nav.Cumulative_Strategy_Returns[-2]) , 2 ))
+    st.sidebar.text('Nav: {}'.format(round(model.nav.Cumulative_Strategy_Returns[-2]) , 3 ))
