b9e928b69aba9d3d6a15c8c4e545ad02148dfdf5
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-13 19:00:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,8 @@ class Run_model :
         nav_dataset = self.deep
         nav_dataset['Next_Returns'] = np.log(nav_dataset['OHLC4']/nav_dataset['OHLC4'].shift(1))
         nav_dataset['Next_Returns'] = nav_dataset['Next_Returns'].shift(-1)
+        nav_dataset['CumulativeMarket_Returns'] = np.cumsum(nav_dataset['Next_Returns'])
+        #__________        
         nav_dataset['Strategy_Returns'] = np.where(nav_dataset['Predict'] == True  , nav_dataset['Next_Returns']  , -nav_dataset['Next_Returns'] )
         nav_dataset['Cumulative_Returns'] = np.cumsum(nav_dataset['Strategy_Returns'])
         #__________
@@ -123,8 +125,8 @@ class Run_model :
         nav_dataset = nav_dataset.iloc[: , 5:].drop(columns=['y_Reg'])
         plt.figure(figsize=(12,8))
         plt.plot(nav_dataset['Cumulative_Returns'], color='k',  alpha=0.60 )
-        plt.plot(nav_dataset['CumulativeMax_Returns'], color='r',  alpha=0.60 )
-        
+        plt.plot(nav_dataset['CumulativeMax_Returns'], color='g',  alpha=0.60 )
+        plt.plot(nav_dataset['CumulativeMarket_Returns'], color='r',  alpha=0.60 )
         st.write('Score:' , round((nav_dataset.Cumulative_Returns[-2]) , 4 ))
         st.pyplot()
         nav_dataset = nav_dataset.dropna()
